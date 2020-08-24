c953324380d026c2b8a52a2eb0f9969eb027f42c
GSLevel <67515314+GSLevel@users.noreply.github.com>
2020-08-24 18:43:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ var closebutton = document.getElementById('closeMenuBtn');
 var links = document.getElementById('links');
 var reqs = document.getElementById('reqs');
 var everything = document.getElementById('everything');
+var ign = document.getElementById('ignInput');
+var tag = document.getElementById('tagInput');
+var shot = document.getElementById('shotInput');
+var checkBtn = document.getElementById('check');
+var submit = document.querySelector('.submit');
 
 function menuBtn() {
      links.style.display = "unset";
@@ -27,6 +32,29 @@ if(document.cookie == "read=true") {
     reqs.style.display = "none";
 }
 
+function check() {
+   if(ign.value.trim() == "") {
+      checkBtn.innerHTML = "type in your ign";
+      setTimeout(function() {
+          checkBtn.innerHTML = "Check";
+      }, 2000);
+   } else if(tag.value.trim() == "") {
+      checkBtn.innerHTML = "type in your discord tag";
+      setTimeout(function() {
+        checkBtn.innerHTML = "Check";
+    }, 2000);
+   } else if(shot.value.trim() == "") {
+      checkBtn.innerHTML = "type in the screenshot url of your stats";
+      setTimeout(function() {
+        checkBtn.innerHTML = "Check";
+    }, 2000);
+   } else {
+       checkBtn.style.display = "none";
+       submit.style.display = "unset";
+   }
+}
+
 reqs.addEventListener("click", reqBtnClicked);
 button.onclick = menuBtn;
 closebutton.onclick = closeBtn;
+checkBtn.onclick = check;
